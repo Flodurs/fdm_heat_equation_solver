1c10daf1edb740ea90d900da72de706a538e9316
Flodurs <76902177+Flodurs@users.noreply.github.com>
2024-05-12 15:32:52 +0200

    push

--- wave_equation_solver.py
+++ wave_equation_solver.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-size_x = 100
-size_y = 100
+size_x = 300
+size_y = 300
 
 h = 0.2
 dt = 0.001
@@ -19,7 +19,13 @@ U_buf = np.zeros((size_x,size_y))
 
 plt.ion()
 
-U[:][0] = 100
+
+
+# for i in range(3):
+        # for j in range(3):
+            # U[i+50][j+50] = 100
+            
+U[150][150] = 100
 
 for step in range(1000000):
     for i in range(1,size_x-1):
@@ -32,13 +38,17 @@ for step in range(1000000):
     U_minus_eins = np.array(U, copy=True)        
     U = np.array(U_buf, copy=True) 
     
-        
+    # for i in range(45):
+        # U[50][i] = 0
+        # U[50][i+55] = 0    
     
   
     print(step)
-    plt.imshow(U,cmap="hot")
-    plt.show()
+    if step%2 == 0:
+        plt.clf()      
+        plt.imshow(U,cmap="copper")
+        plt.show()
     plt.pause(0.001)
-    plt.clf()      
+    
     
     
